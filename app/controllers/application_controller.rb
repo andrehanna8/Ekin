0f04f0b9932ba2fd69c4b7b66f421d9ff3096ef2
andrehanna8 <andrehanna3@gmail.com>
2023-02-21 01:37:30 -0800

    fixing auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,57 @@
 class ApplicationController < ActionController::API
-    before_action :snake_case_params
+    before_action :snake_case_params #, :attatch_authenticity_token
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def login!(user)
+        @current_user = user
+        if user 
+            session[:session_token] = user.reset_session_token!
+        end
+    end
+
+    def logout!
+        if @current_user
+            @current_user.reset_session_token!
+        end
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def require_logged_in
+        unless current_user
+            render json: { message: 'Unauthorized' }, status: :unauthorized 
+        end
+    end
+
+    def test
+        if params.has_key?(:login)
+          login!(User.first)
+        elsif params.has_key?(:logout)
+          logout!
+        end
+      
+        if current_user
+          render json: { user: current_user.slice('id', 'username', 'session_token') }
+        else
+          render json: ['No current user']
+        end
+      end
 
     private
     
     def snake_case_params
       params.deep_transform_keys!(&:underscore)
     end
+
+    # def attatch_authenticity_token
+    #     headers["X-CSRF-Token"] = masked_authenticity_token(session)
+    # end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -34,9 +34,11 @@ class User < ApplicationRecord
 
   def reset_session_token!
     self.session_token = generate_unique_session_token
-    return self.session_token
+    self.save!
+    self.session_token
   end
   private
+  
   def generate_unique_session_token
     loop do
       session_token = SecureRandom::urlsafe_base64
