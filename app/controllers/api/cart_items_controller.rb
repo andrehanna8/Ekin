bb19b9d61cde0840cf6b619d53f937d27610639c
andrehanna8 <andrehanna3@gmail.com>
2023-04-05 17:40:28 -0700

    added quantity update

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -13,14 +13,28 @@ class Api::CartItemsController < ApplicationController
     end
     
     def create
-        @cart_item = CartItem.new(cart_item_params)
-        @cart_item.user_id = current_user.id
-        if @cart_item.save
-        render :show
+        @cart_item = CartItem.find_by(user_id: current_user.id, product_id: cart_item_params[:product_id], options: cart_item_params[:options])
+      
+        if @cart_item
+          # If the item exists, update the quantity
+          @cart_item.quantity += 1
+          if @cart_item.save
+            render :show
+          else
+            render json: @cart_item.errors.full_messages, status: 422
+          end
         else
-        render json: @cart_item.errors.full_messages, status: 422
+          # If the item doesn't exist, create a new cart item
+          @cart_item = CartItem.new(cart_item_params)
+          @cart_item.user_id = current_user.id
+          if @cart_item.save
+            render :show
+          else
+            render json: @cart_item.errors.full_messages, status: 422
+          end
         end
-    end
+      end
+      
     
     def update
         @cart_item = CartItem.find_by(id: params[:id])
