185ce56e7ec81de3713400b64752cc820d36e68e
andrehanna8 <andrehanna3@gmail.com>
2023-04-07 21:45:14 -0700

    added banner cycle

--- frontend/src/components/SlideShowBar/index.js
+++ frontend/src/components/SlideShowBar/index.js
@@ -1,9 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 export default function SlideShowBar() {
+  const currentUser = useSelector((state) => state.session.user);
 
-    return (
-        <div className="slide-show-bar">
-        
-        </div>
-    )
+  const handleClick = () => {
+    if (!currentUser) {
+      const signUpButton = document.getElementById('sign-up');
+      signUpButton?.click();
+    }
+  };
+
+  const messages = [
+    <>
+      FREE SHIPPING + RETURNS, FREE MEMBERSHIP, EXCLUSIVE PRODUCTS &nbsp;	
+      <button className="join-now-button" onClick={handleClick}>
+        Join Now
+      </button>
+      !
+    </>,
+    <>
+    'WHY WAIT? TRY STORE PICKUP	Buy online and find a store near you for pick up in less than 2 hours. Shop now.'
+    </>,
+
+    // Add more messages here
+  ];
+
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
+
+  const resetTimer = () => {
+    clearInterval(intervalId);
+    const newIntervalId = setInterval(() => {
+      setCurrentMessageIndex((prevIndex) => (prevIndex + 1) % messages.length);
+    }, 5000);
+    setIntervalId(newIntervalId);
+  };
+
+  useEffect(() => {
+    resetTimer();
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const prevMessage = () => {
+    setCurrentMessageIndex((prevIndex) => (prevIndex - 1 + messages.length) % messages.length);
+    resetTimer();
+  };
+
+  const nextMessage = () => {
+    setCurrentMessageIndex((prevIndex) => (prevIndex + 1) % messages.length);
+    resetTimer();
+  };
+
+  return (
+    <div className="slide-show-bar">
+      <div className="message-container">
+        <button className="slide-nav-button prev-button" onClick={prevMessage}>
+          &lt;
+        </button>
+        <div className="message-wrapper">{messages[currentMessageIndex]}</div>
+        <button className="slide-nav-button next-button" onClick={nextMessage}>
+          &gt;
+        </button>
+      </div>
+    </div>
+  );
 }
